1ac762c3348b53c72208dbb37d100c7609c59507
Steph de Los Estados Unidos <admin@hanfubk.com>
2016-03-11 18:01:25 -0500

    Update with param handling ability

--- urllibRequests.py
+++ urllibRequests.py
@@ -1,5 +1,10 @@
 import urllib.request
+import urllib.parse
 
-def get(urlStr):
-    req = urllib.request.urlopen(urlStr)
+def get(urlStr,params={}):
+    if params == {}:
+        req = urllib.request.urlopen(urlStr)
+    else:
+        reqdata = urllib.request.Request(urlStr,urllib.parse.urlencode(params).encode('ascii'))
+	req = urllib.request.urlopen(reqdata)
     return req.read()
