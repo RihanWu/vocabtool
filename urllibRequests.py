4e6ec60b6208a5a8a4a1168fa8ce6fadb63226aa
Steph de Los Estados Unidos <admin@hanfubk.com>
2016-03-13 15:20:20 -0400

    A working replacement of Request

--- urllibRequests.py
+++ urllibRequests.py
@@ -1,12 +1,43 @@
 import urllib.request
 import urllib.parse
 
+class Response:
+    def __init__(self,data,charset='UTF-8'):
+        self.text = data.decode(charset)
 
-def get(urlStr, params={}):
+def getEncoding(ctstr):
+    return ctstr[ctstr.find('charset')+8:]
+
+def gunzip(data):
+    import gzip
+    return gzip.decompress(data)
+
+def inflate(data):
+    import zlib
+    try:
+        return zlib.decompress(data)
+    except zlib.error:
+        return zlib.decompress(data,-zlib.MAX_WBITS)
+
+def get(urlStr,params={}):
     reqdata = urllib.request.Request(urlStr)
     reqdata.add_header('User-Agent',
                        'VocabTool/0.2 (https://github.com/RihanWu/vocabtool)')
+    reqdata.add_header('Accept-Encoding','gzip, deflate')
     if params != {}:
         reqdata.data = urllib.parse.urlencode(params).encode('ascii')
     req = urllib.request.urlopen(reqdata)
-    return req.read()
+    if req.headers.get('Content-Encoding') is not None:
+        if req.headers.get('Content-Encoding') == 'gzip':
+            result = gunzip(req.read())
+        elif req.headers.get('Content-Encoding') == 'deflate':
+            result = inflate(req.read())
+        else:
+            result = req.read()
+    else:
+        result = req.read()
+    if (req.headers.get('Content-Type') is not None and
+        req.headers.get('Content-Type').find('charset') != -1):
+        return Response(result,getEncoding(req.headers.get('Content-Type')))
+    else:
+        return Response(result)
