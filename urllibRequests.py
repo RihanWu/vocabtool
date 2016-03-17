811640fa2b6c75f53b47991abaa92107f422e099
Rihan Wu <nyoka.wings@gmail.com>
2016-03-17 19:54:35 -0400

    Follow PEP8 and something else
    Follow PEP8
    "Hide" private functions
    Add short docstring

--- urllibRequests.py
+++ urllibRequests.py
@@ -1,53 +1,62 @@
-import sys
-if sys.hexversion < 50331648:
-    import urllib2 as uReq
-    import urllib as uPar
-else:
-    import urllib.request as uReq
-    import urllib.parse as uPar
+"""Use urllib for web communication"""
 
-class Response:
-    def __init__(self,data,charset='UTF-8'):
+from sys import hexversion
+# Check python version
+if hexversion < 0x300000:
+    import urllib2 as req
+    import urllib as par
+else:
+    import urllib.request as req
+    import urllib.parse as par
+
+
+class Response():
+    def __init__(self, data, charset='UTF-8'):
         self.text = data.decode(charset)
 
-def getEncoding(ctstr):
+
+def _get_encoding(ctstr):
     return ctstr[ctstr.find('charset')+8:]
 
-def gunzip(data):
-    if sys.hexversion < 50331648:
+
+def _gunzip(data):
+    if hexversion < 0x300000:
         import zlib
-        return zlib.decompress(data,16+zlib.MAX_WBITS)
+        return zlib.decompress(data, 16 + zlib.MAX_WBITS)
     else:
         import gzip
         return gzip.decompress(data)
 
-def inflate(data):
+
+def _inflate(data):
     import zlib
     try:
         return zlib.decompress(data)
     except zlib.error:
-        return zlib.decompress(data,-zlib.MAX_WBITS)
+        return zlib.decompress(data, -zlib.MAX_WBITS)
 
-def get(urlStr,params={}):
-    reqdata = uReq.Request(urlStr)
+
+def get(urlStr, params={}):
+    """Get content on urlStr with params"""
+
+    reqdata = req.Request(urlStr)
     reqdata.add_header('User-Agent',
                        'VocabTool/0.2 (https://github.com/RihanWu/vocabtool)')
-    reqdata.add_header('Accept-Encoding','gzip, deflate')
+    reqdata.add_header('Accept-Encoding', 'gzip, deflate')
     if params != {}:
-        reqdata.data = uPar.urlencode(params).encode('ascii')
-    req = uReq.urlopen(reqdata)
-    if req.headers.get('Content-Encoding') is not None:
-        print(req.headers.get('Content-Encoding'))
-        if req.headers.get('Content-Encoding') == 'gzip':
-            result = gunzip(req.read())
-        elif req.headers.get('Content-Encoding') == 'deflate':
-            result = inflate(req.read())
+        reqdata.data = par.urlencode(params).encode('ascii')
+    resp = req.urlopen(reqdata)
+    if resp.headers.get('Content-Encoding'):
+        if resp.headers.get('Content-Encoding') == 'gzip':
+            result = _gunzip(resp.read())
+        elif resp.headers.get('Content-Encoding') == 'deflate':
+            result = _inflate(resp.read())
         else:
-            result = req.read()
+            result = resp.read()
     else:
-        result = req.read()
-    if (req.headers.get('Content-Type') is not None and
-        req.headers.get('Content-Type').find('charset') != -1):
-        return Response(result,getEncoding(req.headers.get('Content-Type')))
+        result = resp.read()
+    content_type = resp.headers.get('Content-Type')
+    if (content_type and content_type.find('charset') != -1):
+        return Response(result, _get_encoding(content_type))
     else:
         return Response(result)
