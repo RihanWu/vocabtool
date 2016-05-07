a1bc93c95067eaea31012c8618417aa6aefa53a9
Rihan Wu <nyoka.wings@gmail.com>
2016-05-07 10:30:54 -0400

    Add docstring for cli.py

--- cli.py
+++ cli.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
-"""CLI interface of vocabtool"""
+"""CLI interface of vocabtool.
+
+Arguments:
+    * Lookup
+        * Required:
+            word
+        * Optional:
+            * language ``-l, --language``
+            * source ``-s, --source``
+
+    * Config(sub-command)
+        * enable source ``-e, --enable``
+        * disable source ``-d, --disable``
+        * set key-value pair ``-s, --set``
+
+    * Help
+        * List dictionaries ``--list``
+        * Version ``-v, --version``
+"""
 
 # Import core component
 import core
